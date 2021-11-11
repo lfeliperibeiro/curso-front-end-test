ea83acb5c4cb0a2c6b3152c4390143c58ef83b64
Felipe Ribeiro <piporibeiro@gmail.com>
2021-11-11 19:11:40 -0300

    test: should create a valid quey string when an array is passed as value

--- src/lib/queryStrings.spec.js
+++ src/lib/queryStrings.spec.js
@@ -1,12 +1,19 @@
-const {queryString} = require('./queryStrings')
+const { queryString } = require('./queryStrings');
 
 describe('Object to query string', () => {
-    it('should  create a valid quey string when an object is provided', () => {
-        const obj = {
-            name: 'Felipe',
-            profession: 'developer'
-        }
+  it('should  create a valid quey string when an object is provided', () => {
+    const obj = {
+      name: 'Felipe',
+      profession: 'developer',
+    };
 
-        expect(queryString(obj)).toBe('name=Felipe&profession=developer')
-    });
+    expect(queryString(obj)).toBe('name=Felipe&profession=developer');
+  });
+  it('should create a valid quey string when an array is passed as value', () => {
+    const obj = {
+      name: 'Felipe',
+      abilities: ['JS', 'TDD'],
+    };
+    expect(queryString(obj)).toBe('name=Felipe&abilities=JS,TDD');
+  });
 });
