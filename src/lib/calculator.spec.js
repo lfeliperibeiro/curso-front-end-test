10bfcc90684f6058f187753663764fb028d828d4
Felipe Ribeiro <piporibeiro@gmail.com>
2021-11-11 19:12:28 -0300

    refactor: format using prettier

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,29 +1,29 @@
-const {sum} = require('./calculator')
+const { sum } = require('./calculator');
 
 describe('Calculator', () => {
-    it('should sum 2 and 2 and the result must be 4 ',  () => {
-        expect(sum(2,2)).toBe(4)
-    });
+  it('should sum 2 and 2 and the result must be 4 ', () => {
+    expect(sum(2, 2)).toBe(4);
+  });
 
-    it('should sum 2 and 2 even if of then is a string and to result must be 5 ',  () => {
-        expect(sum('2','2')).toBe(4)
-    });
+  it('should sum 2 and 2 even if of then is a string and to result must be 5 ', () => {
+    expect(sum('2', '2')).toBe(4);
+  });
 
-    it('should  throw an error if what provided to the method cannot be summed', function () {
-        expect(() => {
-            sum('', '2')
-        }).toThrowError()
+  it('should  throw an error if what provided to the method cannot be summed', () => {
+    expect(() => {
+      sum('', '2');
+    }).toThrowError();
 
-        expect(() => {
-            sum([2,2])
-        }).toThrowError()
+    expect(() => {
+      sum([2, 2]);
+    }).toThrowError();
 
-        expect(() => {
-            sum({})
-        }).toThrowError()
+    expect(() => {
+      sum({});
+    }).toThrowError();
 
-        expect(() => {
-            sum()
-        }).toThrowError()
-    });
-})
+    expect(() => {
+      sum();
+    }).toThrowError();
+  });
+});
