6e2ef1e4e34b58d6767946d2aa6ca85c6e40e822
Felipe Ribeiro <piporibeiro@gmail.com>
2021-11-20 21:48:36 -0300

    refactor: move classes for separate file

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -80,7 +80,7 @@ describe('Cart', () => {
       expect(cart.getTotal().getAmount()).toEqual(0);
     });
 
-    it('should return object with the total and the list of items when sumary is called', () => {
+    it('should return object with the total and the list of items when summary is called', () => {
       cart.add({
         product,
         quantity: 2,
@@ -89,7 +89,7 @@ describe('Cart', () => {
         product: product2,
         quantity: 3,
       });
-      expect(cart.sumary()).toMatchSnapshot();
+      expect(cart.summary()).toMatchSnapshot();
       expect(cart.getTotal().getAmount()).toBeGreaterThan(0);
     });
 
@@ -102,7 +102,7 @@ describe('Cart', () => {
         product: product2,
         quantity: 3,
       });
-      expect(cart.sumary().formatted).toEqual('R$3,025.56');
+      expect(cart.summary().formatted).toEqual('R$3,025.56');
     });
   });
 
